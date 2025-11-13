b6f84e84a8466e15ad24b0eaf7a3aaaf37fba745
wawan220 <mail@pohl-waldemar.info>
2025-11-13 08:22:31 +0100

    nothing

---
+++ aufgabe6Funktionen.py
@@ -0,0 +1,6 @@
+"""
+1. schreibe eine Funktion namens 'Kleidung' die je nach eingegebener Temperatur die Kleidung empfielt 
+2. in dieser Funktion soll aber auch nach dem Niederschlag gefragt werden,  
+    und sagen ob man einen Regenschirm braucht oder nicht
+    
+"""
