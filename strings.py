096d87e39eccb8e181b197c29319112d303b49d8
wawan220 <mail@pohl-waldemar.info>
2025-11-14 09:32:59 +0100

    string hinzugefügt

---
+++ strings.py
@@ -0,0 +1,22 @@
+text = "Hallo Welt"
+
+print(text[0])
+print(text[-1])
+print(text[2:])
+print(text[2:5])
+print(text[-6:-1])
+print(text[::1])
+#print(text[start:ende:sritt])
+print(text[2:9:2])
+print(text[::-1])
+print(text[::-3])
+
+tetx1=text.split(" ")[0]
+print(tetx1)
+tetx2=text.split(" ")[1]
+print(tetx2)
+
+program="python_kurs_123"
+
+pr1=program.split("_")
+print(pr1)
