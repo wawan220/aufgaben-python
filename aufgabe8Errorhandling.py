2543a302e3210a10c8c2e7e8c40229ffb199bfa6
wawan220 <mail@pohl-waldemar.info>
2025-11-14 08:45:18 +0100

    Aufgabe 8 TRY / Except / Finaly (Errorhandling) lösung

--- aufgabe8Errorhandling.py
+++ aufgabe8Errorhandling.py
@@ -7,3 +7,27 @@ Schreibe ein kleines program das:
 - weise den benutzer auf die fehler hin und ende das program 
 - 
 """
+import sys
+
+
+try:
+    x=int(input("gebe eine zahl ein: "))
+
+    print(4/x)
+
+
+#except(ZeroDivisionError,ValueError):
+#    Print("gebe eine gültige ganzzahl ein")
+
+except (ZeroDivisionError) :
+    print("gebe eine gültige zahl ein, diese darf nicht 0 sein")
+    print("es ist folgender fehler aufgetreten: ", sys.exc_info())
+
+except (ValueError) :
+    print("gebe eine gültige zahl ein, diese muss eine ganzzahl sein")
+    print("es ist folgender fehler aufgetreten: ", sys.exc_info())
+
+finally:
+    print("starte das program bitte neu")
+
+print("ende")
